faf256c13281d888c0c313df69f1728cb678bcaa
Paulo Eduardo Santos de Sousa <eduardo.lwx@gmail.com>
2020-07-19 09:29:48 -0300

    wip

--- sample/src/main/java/com/eduardolwx/kotlinxt/MainActivity.kt
+++ sample/src/main/java/com/eduardolwx/kotlinxt/MainActivity.kt
@@ -3,8 +3,10 @@ package com.eduardolwx.kotlinxt
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.eduardolwx.androidxt.extensions.goneUnless
 import com.eduardolwx.androidxt.lifecycle.getViewModel
 import com.eduardolwx.androidxt.lifecycle.onChange
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +15,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        txHello.goneUnless(true)
 
         viewModel = getViewModel(MainViewModel::class.java)
 
